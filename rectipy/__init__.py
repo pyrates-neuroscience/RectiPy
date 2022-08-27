bb3ca321817e71db9d01ce6446fdc2ac5b49c5ba
rgast <rgast@cbs.mpg.de>
2022-08-26 20:21:53 -0500

    Bump version: 0.4.1-dev0 → 0.4.1

--- rectipy/__init__.py
+++ rectipy/__init__.py
@@ -29,7 +29,7 @@ Input, recurrent network, and output layer.
 
 __author__ = "Richard Gast"
 __status__ = "Development"
-__version__ = "0.4.1-dev0"
+__version__ = "0.4.1"
 
 from .network import Network
 from .utility import random_connectivity, circular_connectivity, input_connections

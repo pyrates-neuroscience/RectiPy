b8edf3d73103a777a1a66a74d9c8979f849e1e55
Richert <richgast.90@gmail.com>
2023-02-17 17:14:38 -0600

    minor changes to the testing script for the RLS algorithm

--- documentation/rls_example.py
+++ documentation/rls_example.py
@@ -16,75 +16,81 @@ device = "cuda:0"
 
 # model parameters
 node = "model_templates.base_templates.tanh_node"
-N = 20
-J = np.random.uniform(low=-1.0, high=1.0, size=(N, N))*5.0
+N = 100
+k = 1.5
+tau = 10.0
 J0 = np.random.uniform(low=-1.0, high=1.0, size=(N, N))
+J0 /= np.max(np.abs(np.linalg.eigvals(J0)))
 D = np.random.choice([1.0, 2.0, 3.0], size=(N, N))
 S = D*0.3
-dt = 1e-2
+dt = 1e-3
+edge_attr = dict() #{'delay': D, 'spread': S}
 
-# initialize networks
-target_net = Network.from_yaml("neuron_model_templates.rate_neurons.leaky_integrator.tanh", weights=J, dt=dt,
-                               edge_attr={'delay': D, 'spread': S}, source_var="tanh_op/r", target_var="li_op/r_in",
-                               input_var="li_op/I_ext", output_var="li_op/v", clear=True, float_precision="float64",
-                               file_name='target_net', device=device, node_vars={'all/li_op/v': np.random.randn(N)})
+# initialize network
+net = Network.from_yaml("neuron_model_templates.rate_neurons.leaky_integrator.tanh", weights=J0, dt=dt,
+                        edge_attr=edge_attr, source_var="tanh_op/r", target_var="li_op/r_in",
+                        input_var="li_op/I_ext", output_var="li_op/v", clear=True, float_precision="float64",
+                        node_vars={"all/li_op/k": k, "all/li_op/tau": tau, 'all/li_op/v': np.random.randn(N)},
+                        file_name='learning_net', device=device)
 
-learning_net = Network.from_yaml("neuron_model_templates.rate_neurons.leaky_integrator.tanh", weights=J0, dt=dt,
-                                 edge_attr={'delay': D, 'spread': S}, source_var="tanh_op/r", target_var="li_op/r_in",
-                                 input_var="li_op/I_ext", output_var="li_op/v", clear=False, float_precision="float64",
-                                 node_vars={'all/li_op/v': np.random.randn(N)},
-                                 file_name='learning_net', device=device)
-learning_net.add_output_layer(N, train="rls", beta=0.999, delta=10.0)
+# add RLS learning layer
+net.add_output_layer(1, train="rls", beta=0.999, delta=1.0)
+net.compile()
 
-# compile networks
-target_net.compile()
-learning_net.compile()
-
-# create target data
-####################
+# online optimization parameters
+################################
 
 # meta parameters
 tol = 1e-5
 loss = 1.0
-max_steps = 100000
-test_steps = 2000
+max_steps = 1000000
+sample_steps = 100
+test_steps = 100000
 epsilon = 0.999
 
 # input parameters
-freq = 0.01
+freq = 0.05
 amp = 2.0
-W_fb = torch.randn(N, N, device=device, dtype=torch.float64)
+W_fb = torch.randn(N, 1, device=device, dtype=torch.float64)
+
+# define input
+time = np.linspace(0, max_steps*dt, num=max_steps)
+inp = np.sin(2 * np.pi * freq * time) * amp
+target = inp * np.sin(1 * np.pi * freq * time + 0.5*np.pi) * amp
+# plt.plot(time, inp, color="blue", label="input")
+# plt.plot(time, target, color="orange", label="target")
+# plt.legend()
+# plt.show()
 
 # optimization
 ##############
 
 # optimization loop
-losses = []
+losses, train_steps = [], []
 step = 0
-x = torch.zeros((N,), dtype=torch.float64, device=device)
+y_hat = torch.zeros((1,), dtype=torch.float64, device=device)
 while loss > tol and step < max_steps:
 
-    inp = np.sin(2 * np.pi * freq * step * dt) * amp
-    y = target_net.forward(inp)
-    x = learning_net.forward_train_fb(inp, y, W_fb @ x)
-    loss = epsilon*loss + (1-epsilon)*learning_net[-1].loss.detach().cpu().numpy()
-    losses.append(loss)
+    y_hat = net.forward_train_fb(inp[step], target[step], W_fb @ y_hat)
     step += 1
-    print(f"{step} training steps finished. Current loss: {loss}.")
+    if step % sample_steps == 0:
+        loss = epsilon * loss + (1 - epsilon) * net[-1].loss.detach().cpu().numpy()
+        losses.append(loss)
+        train_steps.append(step)
+        print(f"{step} training steps finished. Current loss: {loss}.")
 
 # model testing
 ###############
 
 print("Starting testing...")
+
 predictions = []
 targets = []
-prediction = torch.zeros((N,), dtype=torch.float64, device=device)
+prediction = torch.zeros((1,), dtype=torch.float64, device=device)
 for step in range(test_steps):
-    inp = np.sin(2 * np.pi * freq * step * dt) * amp
-    prediction = learning_net.forward_fb(inp, W_fb @ prediction)
-    target = target_net.forward(inp)
+    prediction = net.forward_fb(inp[step], W_fb @ prediction)
     predictions.append(prediction.detach().cpu().numpy())
-    targets.append(target.detach().cpu().numpy())
+    targets.append(target[step])
 print("Finished.")
 
 # plotting
@@ -102,9 +108,9 @@ ax2.set_title('targets (testing)')
 ax2.set_xlabel('steps')
 ax2.set_ylabel('u')
 ax3 = axes[2]
-ax3.plot(losses)
+ax3.plot(train_steps, losses)
 ax3.set_title('loss (training)')
-ax3.set_xlabel('epochs')
+ax3.set_xlabel('training steps')
 ax3.set_ylabel('MSE')
 plt.tight_layout()
 

282c1938d9b67fb98d7222ccac061f23809eaf52
Richert <richgast.90@gmail.com>
2023-05-06 17:48:16 -0500

    Bump version: 0.10.0 → 0.10.1-dev0

--- rectipy/__init__.py
+++ rectipy/__init__.py
@@ -29,7 +29,7 @@ Input, recurrent network, and output layer.
 
 __author__ = "Richard Gast"
 __status__ = "Development"
-__version__ = "0.10.0"
+__version__ = "0.10.1-dev0"
 
 from .network import Network, Observer
 from .utility import random_connectivity, circular_connectivity, input_connections, normalize
